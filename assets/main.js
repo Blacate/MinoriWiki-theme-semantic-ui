c4b97efcb66c29154e1997f8aa9d5a62b715a6ca
Blacate <blacated@gmail.com>
2018-06-15 00:13:33 +0800

    change personal info & remove using js to handle table style

--- assets/main.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-	let mainContent = document.querySelector('.page-content');
-	let tables = mainContent.querySelectorAll('table');
-	for(table of tables) {
-		table.className = "ui celled table";
-	}
-}
